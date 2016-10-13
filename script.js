110b2f060cd27a314360f2ce7a76bb302e52d5b1
monseroad <monsepino@gmail.com>
2016-10-12 23:32:44 -0300

    leccion20

---
+++ script.js
@@ -0,0 +1,10 @@
+// tu codigo va aca
+
+function callbackPromedio (a, b, c, callback){
+    var p = (a + b +c)/3;
+	callbackPromedio = a.toString() + "+" + b.toString() + "+" + c.toString + "/" + 3 + "=" + p.toString ;
+   
+     return callbackPromedio; 
+}
+
+  
